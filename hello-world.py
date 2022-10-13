5d1dd626085bafd791cc5416e1ca74af803658b6
fajri <fajri060801@gmail.com>
2022-10-13 20:53:40 +0700

    penambahan var input untuk nama dan kelas

--- hello-world.py
+++ hello-world.py
@@ -1,3 +1,9 @@
 print('Hello World !')
 print('\nNama Saya Muhammad Fajri Al Banna')
-print('\nBerasal dari kelas ASIMO')   
+print('\nBerasal dari kelas ASIMO')
+
+# untuk lebih singkat
+nama = input('Masukkan Nama Anda \t: ')
+kelas = input('Masukkan Kelas Anda \t: ')
+
+print('Halo nama saya {}, dan saya berasal dari kelas {}. Saya sangat senang belajar di Orbit Future Academy'.format(nama, kelas))
